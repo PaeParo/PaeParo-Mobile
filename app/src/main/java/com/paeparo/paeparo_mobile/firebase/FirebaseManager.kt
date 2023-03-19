226b0e62e5af68922703e5d9edf9b66f65701829
Sohn Young Jin <sonkim1001@naver.com>
2023-03-19 21:19:53 +0900

    feat: Firebase에 관한 연결을 관리하는 FirebaseManager 클래스 추가

---
+++ app/src/main/java/com/paeparo/paeparo_mobile/firebase/FirebaseManager.kt
@@ -0,0 +1,22 @@
+package com.paeparo.paeparo_mobile.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.storage.FirebaseStorage
+
+class FirebaseManager {
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    val realtimeDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+    val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    val storage: FirebaseStorage = FirebaseStorage.getInstance()
+
+    val functions: FirebaseFunctions = FirebaseFunctions.getInstance()
+
+    val messaging: FirebaseMessaging = FirebaseMessaging.getInstance()
+}
