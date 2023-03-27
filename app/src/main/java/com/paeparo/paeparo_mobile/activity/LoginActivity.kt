e60532ba4db3bffe57e4f4d3a7cc147e9db9f90f
Sohn Young Jin <sonkim1001@naver.com>
2023-03-28 00:10:11 +0900

    feat: SplashActivity와 LoginActivity에 구글 로그인 기능 추가 SplashActivity에 자동로그인, LoginActivity에 Google 로그인 버튼 추가
    - SplashActivity에서는 기존에 로그인한 사용자가 있을 경우, 해당 계정으로 자동 로그인 시도 => 성공 시 MainActivity, 실패 시 LoginActivity로 이동
    - LoginActivity에는 Google 로그인 버튼을 누를 경우 연동할 Google 계정 선택 창이 뜨도록 구현

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
@@ -2,16 +2,37 @@ package com.paeparo.paeparo_mobile.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var btnGoogleLogin: ConstraintLayout
+    private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        // Google 로그인 결과를 처리하는 launcher를 LoginActivity에서 생성
+        googleSignInLauncher = FirebaseManager.createGoogleLoginLauncher(this,
+            onSuccess = {
+                Toast.makeText(this, "Firebase auth success", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            onFailure = {
+                Toast.makeText(this, "Firebase auth failed", Toast.LENGTH_SHORT).show()
+            })
+
+        // Firebase Auth 로그인 테스트 코드
+        btnGoogleLogin = findViewById(R.id.btn_google_login)
+        btnGoogleLogin.setOnClickListener {
+            // 구글 로그인 처리
+            FirebaseManager.loginWithGoogle(this, googleSignInLauncher)
+        }
     }
 }
--- app/src/main/java/com/paeparo/paeparo_mobile/activity/SplashActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/SplashActivity.kt
@@ -1,23 +1,48 @@
 package com.paeparo.paeparo_mobile.activity
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
 import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME: Long = 2000
+    private val context: Context = this
+    private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        googleSignInLauncher = FirebaseManager.createGoogleLoginLauncher(context,
+            onSuccess = {
+                Toast.makeText(this, "Auto Login: Firebase auth success", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            onFailure = {
+                Toast.makeText(this, "Auto Login: Firebase auth failed", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+
+        // Google 로그인 결과를 처리하는 launcher를 LoginActivity에서 생성
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (FirebaseManager.auth.currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                FirebaseManager.loginWithGoogle(context, googleSignInLauncher)
+            }
         }, SPLASH_TIME)
     }
 }
