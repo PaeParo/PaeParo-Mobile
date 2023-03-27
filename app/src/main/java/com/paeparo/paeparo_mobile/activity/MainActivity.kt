f26f55f7c88e70fcd27e608d5726bd38491474d2
Sohn Young Jin <sonkim1001@naver.com>
2023-03-28 00:01:34 +0900

    refactor: MainActivity에서 로그인 테스트 코드 제거

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
@@ -1,57 +1,12 @@
 package com.paeparo.paeparo_mobile.activity
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.GoogleAuthProvider
 import com.paeparo.paeparo_mobile.R
-import com.paeparo.paeparo_mobile.manager.FirebaseManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Firebase Auth 로그인 테스트 코드
-        // Google 로그인 셋팅
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        // Google 로그인 창 표시
-        // startForResult.launch(GoogleSignIn.getClient(this, gso).signInIntent)
-    }
-
-    // Google 로그인 결과 처리
-    private val startForResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-    // Google 로그인에서 얻은 ID 토큰으로 Firebase 인증 처리
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        FirebaseManager.auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // 로그인 성공
-                    Toast.makeText(this, "Firebase Authentication Success.", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    // 로그인 실패
-                    Toast.makeText(this, "Firebase Authentication failed.", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
     }
 }
