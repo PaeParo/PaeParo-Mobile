2a1cbe5b4d64fa7dea55ee85b045033e2814196b
Sohn Young Jin <sonkim1001@naver.com>
2023-03-28 03:27:32 +0900

    feat: LoginActivity에 애니메이션 이벤트 추가
    - PaeParo 문구는 상단에서 점점 불투명한 애니메이션 적용
    - 로그인 타이틀 문구는 하단에서 점점 불투명한 애니메이션 적용
    - Google 연동 버튼은 점점 붙루명한 애니메이션 적용

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
@@ -2,6 +2,11 @@ package com.paeparo.paeparo_mobile.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +15,23 @@ import com.paeparo.paeparo_mobile.R
 import com.paeparo.paeparo_mobile.manager.FirebaseManager
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var ivPaeParoLogo: ImageView
+    private lateinit var tvPaeParo: TextView
+    private lateinit var tvLoginTitle: TextView
     private lateinit var btnGoogleLogin: ConstraintLayout
     private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(0, 0)
         setContentView(R.layout.activity_login)
+
+        ivPaeParoLogo = findViewById(R.id.iv_paeparo_logo)
+        tvPaeParo = findViewById(R.id.tv_paeparo)
+        tvLoginTitle = findViewById(R.id.tv_login_title)
+        btnGoogleLogin = findViewById(R.id.btn_google_login)
+
+        startAnimation()
 
         // Google 로그인 결과를 처리하는 launcher를 LoginActivity에서 생성
         googleSignInLauncher = FirebaseManager.createGoogleLoginLauncher(this,
@@ -29,10 +46,45 @@ class LoginActivity : AppCompatActivity() {
             })
 
         // Firebase Auth 로그인 테스트 코드
-        btnGoogleLogin = findViewById(R.id.btn_google_login)
         btnGoogleLogin.setOnClickListener {
             // 구글 로그인 처리
             FirebaseManager.loginWithGoogle(this, googleSignInLauncher)
         }
+    }
+
+    private fun startAnimation(){
+        tvPaeParo.alpha = 0f
+        tvPaeParo.translationY = -dpToPx(16f)
+
+        tvLoginTitle.alpha = 0f
+        tvLoginTitle.translationY = dpToPx(16f)
+
+        btnGoogleLogin.alpha = 0f
+
+        tvPaeParo.animate()
+            .translationY(0f)
+            .alpha(1f)
+            .setDuration(2000)
+            .setInterpolator(DecelerateInterpolator())
+            .start()
+        tvLoginTitle.animate()
+            .translationY(0f)
+            .alpha(1f)
+            .setDuration(2000)
+            .setInterpolator(DecelerateInterpolator())
+            .start()
+        btnGoogleLogin.animate()
+            .alpha(1f)
+            .setDuration(2000)
+            .setInterpolator(LinearInterpolator())
+            .start()
+    }
+
+    private fun dpToPx(dp: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            resources.displayMetrics
+        )
     }
 }
