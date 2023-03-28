55232a29a77f91dcd873603f8ee9383503a13b48
Sohn Young Jin <sonkim1001@naver.com>
2023-03-28 18:40:30 +0900

    feat: User 객체를 Map 형식으로 변환하는 toMap() 함수 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/model/User.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/model/User.kt
@@ -8,7 +8,19 @@ data class User(
     val trips: List<String> = listOf(),
     val likedPosts: List<String> = listOf(),
     val commentedPosts: List<CommentedPost> = listOf()
-)
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "nickname" to nickname,
+            "age" to age,
+            "gender" to gender,
+            "travelStyle" to travelStyle,
+            "trips" to trips,
+            "likedPosts" to likedPosts,
+            "commentedPosts" to commentedPosts
+        )
+    }
+}
 
 data class CommentedPost(
     val postId: String = "",
