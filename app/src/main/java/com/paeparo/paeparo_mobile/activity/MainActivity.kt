5d672d0bcc1baedff06c8906d2587b4d5b1f7477
seo0j <seo0jjjjj@gmail.com>
2023-03-28 23:24:26 +0900

    feat BottomNavBar 기능추가

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
@@ -2,11 +2,42 @@ package com.paeparo.paeparo_mobile.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
 import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.fragment.MyHomeFragment
+import com.paeparo.paeparo_mobile.fragment.PlanFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navbar = findViewById<NavigationBarView>(R.id.bottomNavigationView)
+
+        // Navigation 선택 이벤트
+        navbar.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+            val ft = supportFragmentManager.beginTransaction()
+            when(item.itemId) {
+                R.id.home_fragment -> {
+                    //ft.replace(R.id.frameLayout,MyHomeFragment()).commit()
+                    true
+                }
+                R.id.community_fragment -> {
+                    //ft.replace(R.id.frameLayout,MyHomeFragment()).commit()
+                    true
+                }
+                R.id.plan_fragment -> {
+                    ft.replace(R.id.frameLayout,PlanFragment()).commit()
+                    true
+                }
+                R.id.mypage_fragment -> {
+                    ft.replace(R.id.frameLayout,MyHomeFragment()).commit()
+                    true
+                }
+                else -> false
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanFragment.kt
@@ -1,18 +1,28 @@
 package com.paeparo.paeparo_mobile.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.paeparo.paeparo_mobile.R
 
-class MyHomeFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PlanFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class PlanFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_home, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plan, container, false)
     }
+
+
+
 }
