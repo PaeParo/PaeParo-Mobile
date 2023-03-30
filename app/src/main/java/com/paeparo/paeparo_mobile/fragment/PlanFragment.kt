092454fca2756032db6a6aac46b2fd42c9afdd8f
seo0j <seo0jjjjj@gmail.com>
2023-03-30 14:00:14 +0900

    feat: PlanFragment 일정생성 버튼추가

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanFragment.kt
@@ -1,10 +1,13 @@
 package com.paeparo.paeparo_mobile.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.paeparo.paeparo_mobile.PlanGenerateActivity
 import com.paeparo.paeparo_mobile.R
 
 /**
@@ -23,6 +26,12 @@ class PlanFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_plan, container, false)
     }
 
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.btn_fragment_plan)
+        button.setOnClickListener {
+            val intent = Intent(context,PlanGenerateActivity::class.java)
+              startActivity(intent)
+        }
+    }
 }
