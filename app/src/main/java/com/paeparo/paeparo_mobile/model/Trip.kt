6f7ea3bfb03c91aa14bb8586cd6be7bd1e935c63
Sohn Young Jin <sonkim1001@naver.com>
2023-03-30 14:30:33 +0900

    refactor: Distribution 클래스들에 있는 val 형을 var 형으로 변경

--- app/src/main/java/com/paeparo/paeparo_mobile/model/Trip.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/model/Trip.kt
@@ -14,27 +14,27 @@ data class Trip(
 )
 
 data class GenderDistribution(
-    val male: Int = 0,
-    val female: Int = 0
+    var male: Int = 0,
+    var female: Int = 0
 )
 
 data class AgeDistribution(
-    val `10s`: Int = 0,
-    val `20s`: Int = 0,
-    val `30s`: Int = 0,
-    val `40s`: Int = 0,
-    val `50s`: Int = 0,
-    val `60s`: Int = 0
+    var `10s`: Int = 0,
+    var `20s`: Int = 0,
+    var `30s`: Int = 0,
+    var `40s`: Int = 0,
+    var `50s`: Int = 0,
+    var `60s`: Int = 0
 )
 
 data class TravelPreferences(
-    val activity: Int = 0,
-    val scenery: Int = 0,
-    val relaxation: Int = 0,
-    val luxury: Int = 0,
-    val costEffectiveness: Int = 0,
-    val accommodation: Int = 0,
-    val food: Int = 0,
-    val transportation: Int = 0,
-    val shopping: Int = 0
+    var activity: Int = 0,
+    var scenery: Int = 0,
+    var relaxation: Int = 0,
+    var luxury: Int = 0,
+    var costEffectiveness: Int = 0,
+    var accommodation: Int = 0,
+    var food: Int = 0,
+    var transportation: Int = 0,
+    var shopping: Int = 0
 )
