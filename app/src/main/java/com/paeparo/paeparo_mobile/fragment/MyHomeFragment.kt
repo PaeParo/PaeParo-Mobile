093bbb818d1df6c0b2327d5a796763694499b962
Blen <blenbre777@naver.com>
2023-03-31 22:09:54 +0900

    feat:클릭 이벤트 생성
    -viewBinding 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
@@ -5,14 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.dynamic.SupportFragmentWrapper
 import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.databinding.FragmentMyHomeBinding
 
 class MyHomeFragment : Fragment() {
-
+    private var binding:FragmentMyHomeBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_home, container, false)
+        binding=FragmentMyHomeBinding.inflate(inflater)
+
+        binding!!.fixhome.setOnClickListener{
+            binding!!.name.text="fixhome"
+        }
+
+        binding!!.settings.setOnClickListener{
+            binding!!.name.text="settings"
+        }
+
+        binding!!.plan.setOnClickListener{
+            binding!!.name.text="plan"
+        }
+
+        binding!!.profile.setOnClickListener{
+            binding!!.name.text="profile"
+        }
+
+        binding!!.faq.setOnClickListener{
+            binding!!.name.text="faq"
+        }
+        binding!!.comment.setOnClickListener {
+            binding!!.name.text="comment"
+        }
+
+        binding!!.like.setOnClickListener{
+            binding!!.name.text="like"
+        }
+
+        return binding!!.root
     }
 }
