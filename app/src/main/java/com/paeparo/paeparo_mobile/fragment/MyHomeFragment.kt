437bc8447e4a067709321645dc0626279653fb26
Blen <blenbre777@naver.com>
2023-04-02 12:11:56 +0900

    feat:MyHomePage context 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
@@ -1,23 +1,42 @@
 package com.paeparo.paeparo_mobile.fragment
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import com.google.android.gms.dynamic.SupportFragmentWrapper
-import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.activity.MainActivity
+import com.paeparo.paeparo_mobile.activity.SplashActivity
 import com.paeparo.paeparo_mobile.databinding.FragmentMyHomeBinding
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
+import com.paeparo.paeparo_mobile.model.User
+import kotlinx.coroutines.*
+
 
 class MyHomeFragment : Fragment() {
     private var _binding:FragmentMyHomeBinding? = null
     private val binding get()=_binding!!
+    private val networkScope = CoroutineScope(Dispatchers.IO)
+    lateinit var mainActivity: MainActivity
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        networkScope.launch {
+            val result: Result<User> = FirebaseManager.getCurrentUserData(mainActivity)
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding=FragmentMyHomeBinding.inflate(inflater)
 
         binding!!.fixhome.setOnClickListener{
@@ -34,6 +53,8 @@ class MyHomeFragment : Fragment() {
 
         binding!!.profile.setOnClickListener{
             binding!!.name.text="profile"
+            //val intent = Intent()
+            //startActivity(intent)
         }
 
         binding!!.faq.setOnClickListener{
