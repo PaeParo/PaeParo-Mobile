35a809bf04b5896fc7076008103dde0ce08cbdcf
Blen <blenbre777@naver.com>
2023-04-02 12:23:56 +0900

    style:MyHomePage 코드 정리

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
@@ -1,15 +1,12 @@
 package com.paeparo.paeparo_mobile.fragment
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.paeparo.paeparo_mobile.activity.MainActivity
-import com.paeparo.paeparo_mobile.activity.SplashActivity
+import com.paeparo.paeparo_mobile.activity.MyHome
 import com.paeparo.paeparo_mobile.databinding.FragmentMyHomeBinding
 import com.paeparo.paeparo_mobile.manager.FirebaseManager
 import com.paeparo.paeparo_mobile.model.User
@@ -20,17 +17,12 @@ class MyHomeFragment : Fragment() {
     private var _binding:FragmentMyHomeBinding? = null
     private val binding get()=_binding!!
     private val networkScope = CoroutineScope(Dispatchers.IO)
-    lateinit var mainActivity: MainActivity
 
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mainActivity = context as MainActivity
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         networkScope.launch {
-            val result: Result<User> = FirebaseManager.getCurrentUserData(mainActivity)
+            val result: Result<User> = FirebaseManager.getCurrentUserData(this@MyHomeFragment.requireContext())
+            println(result)
         }
     }
     override fun onCreateView(
@@ -52,8 +44,7 @@ class MyHomeFragment : Fragment() {
         }
 
         binding!!.profile.setOnClickListener{
-            binding!!.name.text="profile"
-            //val intent = Intent()
+            //val intent = Intent(activity,MyHome::class.java)
             //startActivity(intent)
         }
 
