68fd48b923c44aa1a83b464910ddebf072802773
서윤오 <seo0jjjjj@gmail.com>
2023-04-03 22:07:42 +0900

    chore: material-calendarview 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanDateFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanDateFragment.kt
@@ -14,39 +14,26 @@ import com.paeparo.paeparo_mobile.databinding.FragmentPlanDateBinding
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlanDateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlanDateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private var _binding: FragmentPlanDateBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentPlanDateBinding.inflate(inflater,container,false)
+        _binding = FragmentPlanDateBinding.inflate(inflater,container,false) //set binding
+
+
         binding.btnFragmentname.setOnClickListener{
             val vp = activity?.findViewById<ViewPager2>(R.id.vp_plan_generate)
             vp?.setCurrentItem(1,true)
         }
-
-
         return binding.root
     }
 
@@ -55,23 +42,4 @@ class PlanDateFragment : Fragment() {
         super.onDestroyView()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlanDateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlanDateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
