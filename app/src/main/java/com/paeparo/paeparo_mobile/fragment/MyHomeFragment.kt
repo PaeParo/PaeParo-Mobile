6049abccb896f4d9f1357373fa8eefc9f9d9b204
서윤오 <seo0jjjjj@gmail.com>
2023-04-03 20:26:50 +0900

    feat: MyHomeFragment LOG 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/MyHomeFragment.kt
@@ -2,10 +2,12 @@ package com.paeparo.paeparo_mobile.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.paeparo.paeparo_mobile.activity.*
 import com.paeparo.paeparo_mobile.databinding.FragmentMyHomeBinding
 import com.paeparo.paeparo_mobile.manager.FirebaseManager
@@ -17,12 +19,17 @@ class MyHomeFragment : Fragment() {
     private var _binding:FragmentMyHomeBinding? = null
     private val binding get()=_binding!!
     private val networkScope = CoroutineScope(Dispatchers.IO)
-
+    private lateinit var username : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var user : User
         networkScope.launch {
             val result: Result<User> = FirebaseManager.getCurrentUserData(this@MyHomeFragment.requireContext())
-
+            result.onSuccess {
+                user = it as User
+                username = user.nickname
+                Log.d("USERNAME:",username)
+           }
         }
     }
     override fun onCreateView(
@@ -34,6 +41,7 @@ class MyHomeFragment : Fragment() {
         binding!!.fixhome.setOnClickListener{
             val intent = Intent(activity, MyHomeProfileActivity::class.java)
             startActivity(intent)
+            Log.d("USERNAME:",username)
         }
 
         binding!!.settings.setOnClickListener{
