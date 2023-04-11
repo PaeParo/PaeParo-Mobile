f901cf62119a526887fe28643dc76c297d6a56f7
Blen <blenbre777@naver.com>
2023-04-11 23:25:25 +0900

    feat:MyHomeFaq 구현
    -arrow_up 화살표 추가 및 코드 Activity binding 처리

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeFaqActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeFaqActivity.kt
@@ -2,11 +2,29 @@ package com.paeparo.paeparo_mobile.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.paeparo.paeparo_mobile.R
+import android.view.View
+import com.paeparo.paeparo_mobile.databinding.ActivityMyHomeFaqBinding
 
 class MyHomeFaqActivity : AppCompatActivity() {
+    private val binding: ActivityMyHomeFaqBinding by lazy { ActivityMyHomeFaqBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_home_faq)
+        setContentView(binding.root)
+
+        binding.layout01.setOnClickListener {
+            if(binding.layoutDetail01.visibility == View.VISIBLE) {
+                binding.layoutDetail01.visibility = View.GONE
+                binding.layoutBtn01.animate().apply {
+                    duration = 300
+                    rotation(0f)
+                }
+            } else {
+                binding.layoutDetail01.visibility = View.VISIBLE
+                binding.layoutBtn01.animate().apply {
+                    duration = 300
+                    rotation(180f)
+                }
+            }
+        }
     }
 }
