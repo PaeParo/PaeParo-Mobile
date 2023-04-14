faf77fb95843655cfcfab46bfbf75759a81cd63e
Sohn Young Jin <sonkim1001@naver.com>
2023-04-14 17:41:36 +0900

    feat: LocationUpdate, LocationUpdateInfo에 map 형식으로 변경하는 함수 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/model/LocationUpdate.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/model/LocationUpdate.kt
@@ -2,14 +2,25 @@ package com.paeparo.paeparo_mobile.model
 
 import com.google.firebase.firestore.GeoPoint
 import com.google.gson.annotations.SerializedName
+import com.paeparo.paeparo_mobile.util.FirestoreNamingUtil
 
 data class LocationUpdate(
     @SerializedName("trip_id") var tripId: String = "",
     @SerializedName("member_locations") var memberLocations: List<LocationUpdateInfo> = listOf()
-)
+) {
+    fun toMapWithoutTripId(): Map<String, Any?> {
+        val serializedMap = FirestoreNamingUtil.toSerializedMap(this)
+        return serializedMap.filterKeys { it != "trip_id" }
+    }
+}
 
 data class LocationUpdateInfo(
     @SerializedName("user_id") var userId: String = "",
     @SerializedName("timestamp") var timestamp: Long = 0L,
     @SerializedName("location") var location: GeoPoint = GeoPoint(0.0, 0.0)
-)
+) {
+    fun toMapWithouUserId(): Map<String, Any?> {
+        val serializedMap = FirestoreNamingUtil.toSerializedMap(this)
+        return serializedMap.filterKeys { it != "trip_id" }
+    }
+}
