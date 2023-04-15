61c46a97228a121e4f1fc9e211d3e0a881247879
Sohn Young Jin <sonkim1001@naver.com>
2023-04-15 16:30:57 +0900

    feat: FirebaseManager의 결과 및 종류, 반환 데이터를 담고 있는 FirebaseResult 클래스 추가

---
+++ app/src/main/java/com/paeparo/paeparo_mobile/model/FirebaseResult.kt
@@ -0,0 +1,27 @@
+package com.paeparo.paeparo_mobile.model
+
+import com.paeparo.paeparo_mobile.constant.FirebaseConstants
+
+data class FirebaseResult<T>(
+    val isSuccess: Boolean,
+    val type: String? = null,
+    val data: T? = null
+) {
+    companion object {
+        fun <T> success(type: String? = null, data: T? = null): FirebaseResult<T> {
+            return FirebaseResult(true, type, data)
+        }
+
+        fun <T> make(result: Map<*, *>): FirebaseResult<T> {
+            return FirebaseResult(
+                result["result"] as String == FirebaseConstants.ResponseCodes.SUCCESS,
+                result["type"] as String,
+                null
+            )
+        }
+
+        fun <T> failure(type: String? = null): FirebaseResult<T> {
+            return FirebaseResult(false, type, null)
+        }
+    }
+}
