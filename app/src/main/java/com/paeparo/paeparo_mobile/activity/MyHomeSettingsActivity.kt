a2c39f48b97ae433147a3eafbb607e10c2f88b8d
Blen <blenbre777@naver.com>
2023-04-23 05:56:36 +0900

    feat:settings 로그아웃 기능 구현

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
@@ -1,12 +1,37 @@
 package com.paeparo.paeparo_mobile.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.constant.SharedPreferencesKey
+import com.paeparo.paeparo_mobile.databinding.ActivityMyHomeSettingsBinding
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
+import com.paeparo.paeparo_mobile.manager.SharedPreferencesManager
+
+
 
 class MyHomeSettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyHomeSettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_home_settings)
+        binding = ActivityMyHomeSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadData()
+
+        if(!(binding.switch1.isChecked)){
+            SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_TEST,true)
+        }else{
+            SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_TEST,false)
+        }
+        binding.logoutButton.setOnClickListener(){
+            FirebaseManager.logoutWithGoogle(this)
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    //ShardPreferencesKey 로드
+    private fun loadData(){
+        binding.switch1.isChecked=SharedPreferencesManager.get(SharedPreferencesKey.KEY_SETTINGS_TEST,false) ?: false
     }
 }
--- app/src/main/java/com/paeparo/paeparo_mobile/constant/SharedPreferencesKey.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/constant/SharedPreferencesKey.kt
@@ -4,5 +4,6 @@ class SharedPreferencesKey {
     companion object {
         const val KEY_FIRST_LAUNCH = "first_launch"
         const val KEY_TUTORIAL_COMPLETE = "tutorial_complete"
+        const val KEY_SETTINGS_TEST = "test"
     }
 }
