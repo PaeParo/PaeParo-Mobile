ad0d6a21f7769b8e5975e6f35085e47ba69e3c00
Blen <blenbre777@naver.com>
2023-04-24 23:19:41 +0900

    feat:MyHomeProfileActivity 갤러리에서 사진 가져오는 기능 추가
    -프로필 클릭시 이벤트 발생하게 만듦

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeProfileActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeProfileActivity.kt
@@ -1,12 +1,48 @@
 package com.paeparo.paeparo_mobile.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.paeparo.paeparo_mobile.R
+import android.provider.MediaStore
+import android.util.Log
+import com.paeparo.paeparo_mobile.databinding.ActivityMyHomeProfileBinding
 
 class MyHomeProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyHomeProfileBinding
+
+    private val OPEN_GALLERY = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_home_profile)
+        binding = ActivityMyHomeProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //프로필을 클릭했을 경우 갤러리로 이동
+        binding.profileImageView.setOnClickListener(){
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent,OPEN_GALLERY)
+        }
+    }
+
+    //갤러리 이동 후 선택한 이미지 가져와서 적용
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == OPEN_GALLERY){
+                val currentImageUrl : Uri? = data?.data
+                try{
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,currentImageUrl)
+                    binding.profileImageView.setImageBitmap(bitmap)
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }else{
+            Log.d("MyHomeProfile","MyHomeProfileFail")
+        }
     }
 }
