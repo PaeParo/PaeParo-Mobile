d0930c89de1ae6cb34bb6932d813b20a4b585bd8
Blen <blenbre777@naver.com>
2023-05-04 08:44:20 +0900

    style:MyHomeSetting 스타일 변경
    -color #808080 회색 추가
    -SharedPreferences 이름 수정
    -string 알람, 버전 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
@@ -3,6 +3,7 @@ package com.paeparo.paeparo_mobile.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import com.paeparo.paeparo_mobile.constant.SharedPreferencesKey
 import com.paeparo.paeparo_mobile.databinding.ActivityMyHomeSettingsBinding
 import com.paeparo.paeparo_mobile.manager.FirebaseManager
@@ -19,11 +20,24 @@ class MyHomeSettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
         loadData()
 
-        if(!(binding.switch1.isChecked)){
-            SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_TEST,true)
+        //초기 세팅 추후에 코드 바꿀 예정
+        if(binding.alram.isChecked==true){
+            binding.alramDetail.setText("알람을 받습니다.")
         }else{
-            SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_TEST,false)
+            binding.alramDetail.setText("알람을 받지 않습니다.")
         }
+
+        //알람 스위치
+        binding.alram.setOnCheckedChangeListener {_,isChecked ->
+            if(isChecked){
+                binding.alramDetail.setText("알람을 받습니다.")
+                SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_ALARM,true)
+            }else{
+                binding.alramDetail.setText("알람을 받지 않습니다.")
+                SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_ALARM,false)
+            }
+        }
+
         binding.logoutButton.setOnClickListener(){
             FirebaseManager.logoutWithGoogle(this)
             startActivity(Intent(this, LoginActivity::class.java))
@@ -32,6 +46,7 @@ class MyHomeSettingsActivity : AppCompatActivity() {
 
     //ShardPreferencesKey 로드
     private fun loadData(){
-        binding.switch1.isChecked=SharedPreferencesManager.get(SharedPreferencesKey.KEY_SETTINGS_TEST,false) ?: false
+        binding.alram.isChecked=SharedPreferencesManager.get(SharedPreferencesKey.KEY_SETTINGS_ALARM,true) ?: true
+
     }
 }
