04d69bb662c3dee800c4d0c68f526e932c9040a2
Sohn Young Jin <sonkim1001@naver.com>
2023-05-10 23:59:50 +0900

    feat: 내가 속한 여행 목록에 따른 처리 로직 구현 추후 진행 중인 여행, 계획 중인 여행, 지난 여행에 따라서 처리하는 로직 필요

--- app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanEmptyFragment.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/PlanEmptyFragment.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import com.paeparo.paeparo_mobile.activity.PlanGenerateActivity
+import androidx.lifecycle.lifecycleScope
 import com.paeparo.paeparo_mobile.R
+import com.paeparo.paeparo_mobile.activity.PlanGenerateActivity
+import com.paeparo.paeparo_mobile.application.getPaeParo
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
+import kotlinx.coroutines.launch
 
 
 class PlanFragment : Fragment() {
@@ -23,8 +29,29 @@ class PlanFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btn_fragment_plan)
-        button.setOnClickListener {
+
+        lifecycleScope.launch {
+            val userTripsResult = FirebaseManager.getUserTrips(view.context.getPaeParo().userId)
+
+            if (userTripsResult.isSuccess) {
+                val userTrips = userTripsResult.data
+                if (userTrips!!.isEmpty()) { // 내가 속한 여행이 없을 경우
+                    view.findViewById<LinearLayoutCompat>(R.id.ll_plan_empty_view).visibility =
+                        View.VISIBLE
+                    view.findViewById<ConstraintLayout>(R.id.cl_plan_empty_trip_list).visibility =
+                        View.GONE
+                } else { // 내가 속한 여행이 있을 경우
+                    view.findViewById<LinearLayoutCompat>(R.id.ll_plan_empty_view).visibility =
+                        View.GONE
+                    view.findViewById<ConstraintLayout>(R.id.cl_plan_empty_trip_list).visibility =
+                        View.VISIBLE
+                }
+            } else {
+                Toast.makeText(view.context, "여행 목록을 불러오는데 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        view.findViewById<LinearLayoutCompat>(R.id.btn_plan_empty_create_trip).setOnClickListener {
             val intent = Intent(context, PlanGenerateActivity::class.java)
             startActivity(intent)
         }
