9dd07e413df91fe05a57f28edd08f1c3997047af
Sohn Young Jin <sonkim1001@naver.com>
2023-05-11 01:53:57 +0900

    feat: Firebase 관련 통신 부분에 Circular Progress Indicator 추가 및 LoginActivity에 binding 적용

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/LoginActivity.kt
@@ -5,51 +5,46 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.paeparo.paeparo_mobile.R
 import com.paeparo.paeparo_mobile.constant.FirebaseConstants
+import com.paeparo.paeparo_mobile.databinding.ActivityLoginBinding
 import com.paeparo.paeparo_mobile.manager.FirebaseManager
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var ivLoginPaeParoLogo: ImageView
-    private lateinit var tvLoginPaeParo: TextView
-    private lateinit var tvLoginTitle: TextView
-    private lateinit var btnLoginGoogleLogin: ConstraintLayout
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overridePendingTransition(0, 0)
-        setContentView(R.layout.activity_login)
-
-        ivLoginPaeParoLogo = findViewById(R.id.iv_login_paeparo_logo)
-        tvLoginPaeParo = findViewById(R.id.tv_login_paeparo)
-        tvLoginTitle = findViewById(R.id.tv_login_title)
-        btnLoginGoogleLogin = findViewById(R.id.btn_login_google_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         startAnimation()
+
+        binding.ciLoginLoading.visibility = android.view.View.GONE
 
         // Google 로그인 결과를 처리하는 launcher를 LoginActivity에서 생성
         googleSignInLauncher =
             FirebaseManager.createGoogleLoginLauncher(
                 this@LoginActivity,
                 onSuccess = {
+                    binding.ciLoginLoading.visibility = android.view.View.GONE
                     when (it.type) {
                         FirebaseConstants.ResponseCodes.ALL_DATA_SET -> {
                             val intent = Intent(this@LoginActivity, MainActivity::class.java)
                             startActivity(intent)
                             finish()
                         }
+
                         FirebaseConstants.ResponseCodes.NICKNAME_NOT_SET -> {
                             val intent = Intent(this@LoginActivity, NickNameActivity::class.java)
                             startActivity(intent)
                             finish()
                         }
+
                         FirebaseConstants.ResponseCodes.DETAIL_INFO_NOT_SET -> {
                             val intent = Intent(this@LoginActivity, MainActivity::class.java)
                             startActivity(intent)
@@ -58,6 +53,7 @@ class LoginActivity : AppCompatActivity() {
                     }
                 },
                 onFailure = {
+                    binding.ciLoginLoading.visibility = android.view.View.GONE
                     Toast.makeText(
                         this@LoginActivity,
                         "로그인 실패했습니다. 나중에 다시 시도해주세요.",
@@ -67,26 +63,27 @@ class LoginActivity : AppCompatActivity() {
                 })
 
         // Firebase Auth 로그인 테스트 코드
-        btnLoginGoogleLogin.setOnClickListener {
+        binding.btnLoginGoogleLogin.setOnClickListener {
             // 구글 로그인 처리
+            binding.ciLoginLoading.visibility = android.view.View.VISIBLE
             FirebaseManager.launchGoogleLoginTask(this@LoginActivity, googleSignInLauncher)
         }
     }
 
     private fun startAnimation() {
-        tvLoginPaeParo.alpha = 0f
-        tvLoginPaeParo.translationY = -dpToPx(16f)
+        binding.tvLoginPaeparo.alpha = 0f
+        binding.tvLoginPaeparo.translationY = -dpToPx(16f)
 
-        tvLoginTitle.alpha = 0f
-        tvLoginTitle.translationY = dpToPx(16f)
+        binding.tvLoginTitle.alpha = 0f
+        binding.tvLoginTitle.translationY = dpToPx(16f)
 
-        btnLoginGoogleLogin.alpha = 0f
+        binding.btnLoginGoogleLogin.alpha = 0f
 
-        tvLoginPaeParo.animate().translationY(0f).alpha(1f).setDuration(2000)
+        binding.tvLoginPaeparo.animate().translationY(0f).alpha(1f).setDuration(2000)
             .setInterpolator(DecelerateInterpolator()).start()
-        tvLoginTitle.animate().translationY(0f).alpha(1f).setDuration(2000)
+        binding.tvLoginTitle.animate().translationY(0f).alpha(1f).setDuration(2000)
             .setInterpolator(DecelerateInterpolator()).start()
-        btnLoginGoogleLogin.animate().alpha(1f).setDuration(2000)
+        binding.btnLoginGoogleLogin.animate().alpha(1f).setDuration(2000)
             .setInterpolator(LinearInterpolator())
             .start()
     }
