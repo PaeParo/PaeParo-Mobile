ed5fe8035a52209fc78d5b9cb0c52dc3e9b82c65
Sohn Young Jin <sonkim1001@naver.com>
2023-05-14 02:18:03 +0900

    refactor: yyyy-MM-dd -> yyyy.M.d로 Date Format 변경

--- app/src/main/java/com/paeparo/paeparo_mobile/util/DateUtil.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/util/DateUtil.kt
@@ -6,7 +6,7 @@ import java.util.Date
 import java.util.Locale
 
 object DateUtil {
-    val yyyyMMddFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
+    val yyyyMdFormat = SimpleDateFormat("yyyy.M.d", Locale.KOREA)
 
     fun getDateFromTimestamp(timestamp: Timestamp, format: SimpleDateFormat): String {
         return format.format(Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1_000_000))
