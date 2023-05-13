4a7e8bac13ffc6efc4a976771156735e991e4523
Sohn Young Jin <sonkim1001@naver.com>
2023-05-13 19:33:42 +0900

    refactor: Long에서 Timestamp로 변경에 따른 함수 매개변수 및 구조 변경

--- app/src/main/java/com/paeparo/paeparo_mobile/util/DateUtil.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/util/DateUtil.kt
@@ -1,5 +1,6 @@
 package com.paeparo.paeparo_mobile.util
 
+import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -7,7 +8,7 @@ import java.util.Locale
 object DateUtil {
     val yyyyMMddFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
 
-    fun getDateFromLong(date: Long, format: SimpleDateFormat): String {
-        return format.format(Date(date))
+    fun getDateFromTimestamp(timestamp: Timestamp, format: SimpleDateFormat): String {
+        return format.format(Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1_000_000))
     }
 }
