7ca356ab168690edfeff85ddcd4b65c16b8c0396
Sohn Young Jin <sonkim1001@naver.com>
2023-05-14 02:23:41 +0900

    feat: 여행 지역 및 함께 하는 인원 수 표시 구현

--- app/src/main/java/com/paeparo/paeparo_mobile/adapter/TripAdapter.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/adapter/TripAdapter.kt
@@ -9,17 +9,24 @@ import com.paeparo.paeparo_mobile.databinding.ItemTripsHeaderBinding
 import com.paeparo.paeparo_mobile.model.Trip
 import com.paeparo.paeparo_mobile.util.DateUtil
 
-class TripAdapter(private var tripList: List<Any>) :
+/**
+ * TripAdapter class
+ *
+ * TripFragment의 여행 목록을 관리하는 Adapter 클래스
+ *
+ * @property triplist Trip 목록
+ */
+class TripAdapter(private var triplist: List<Any>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val ITEM_TYPE_HEADER = 0
     private val ITEM_TYPE_CONTENT = 1
 
     init {
-        updateTrips(tripList.filterIsInstance<Trip>())
+        updateTrips(triplist.filterIsInstance<Trip>())
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (tripList[position] is String) {
+        return if (triplist[position] is String) {
             ITEM_TYPE_HEADER
         } else {
             ITEM_TYPE_CONTENT
@@ -42,9 +49,9 @@ class TripAdapter(private var tripList: List<Any>) :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is HeaderViewHolder) {
-            holder.bind(tripList[position] as String)
+            holder.bind(triplist[position] as String)
         } else if (holder is ContentViewHolder) {
-            holder.bind(tripList[position] as Trip)
+            holder.bind(triplist[position] as Trip)
         }
     }
 
@@ -60,24 +67,35 @@ class TripAdapter(private var tripList: List<Any>) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(trip: Trip) {
             binding.tvItemTripsContentName.text = trip.name
-            binding.tvItemTripsContentPlace.text = "장소명"
+            binding.tvItemTripsContentPlace.text = trip.region
             binding.tvItemTripsContentDate.text = itemView.context.getString(
                 R.string.date_range,
-                DateUtil.getDateFromTimestamp(trip.startDate, DateUtil.yyyyMMddFormat),
-                DateUtil.getDateFromTimestamp(trip.endDate, DateUtil.yyyyMMddFormat)
+                DateUtil.getDateFromTimestamp(
+                    trip.startDate,
+                    DateUtil.yyyyMdFormat
+                ),
+                DateUtil.getDateFromTimestamp(
+                    trip.endDate,
+                    DateUtil.yyyyMdFormat
+                )
             )
+            binding.tvItemTripsContentMembers.text = itemView.context.resources.getString(
+                R.string.members_count,
+                trip.members.size
+            )
+
             binding.clItemTripsContentInfo.setOnClickListener {
                 // TODO(서윤오): 여행 누를 경우 해당 여행 일정으로 이동
             }
         }
     }
 
-    fun updateTrips(trips: List<Trip>) {
+    fun updateTrips(tripsWithUsersThumbnail: List<Trip>) {
         val sortedTrips =
-            trips.sortedWith(compareByDescending<Trip> { it.status.ordinal }.thenByDescending { it.startDate })
-        val groupedTrips = sortedTrips.groupBy { it.status }
+            tripsWithUsersThumbnail.sortedWith(compareByDescending<Trip> { trip -> trip.status.ordinal }.thenByDescending { trip -> trip.startDate })
+        val groupedTrips = sortedTrips.groupBy { trip -> trip.status }
 
-        tripList = mutableListOf<Any>().apply {
+        this.triplist = mutableListOf<Any>().apply {
             groupedTrips[Trip.TripStatus.ONGOING]?.let { ongoingTrips ->
                 if (ongoingTrips.isNotEmpty()) {
                     add("진행 중인 여행")
@@ -102,6 +120,6 @@ class TripAdapter(private var tripList: List<Any>) :
     }
 
     override fun getItemCount(): Int {
-        return tripList.size
+        return triplist.size
     }
 }
