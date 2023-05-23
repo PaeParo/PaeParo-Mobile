7d9542a0dc52c8ea49c393b0312aca82bd2fc0dc
Sohn Young Jin <sonkim1001@naver.com>
2023-05-24 04:48:55 +0900

    fix: 제거된 멤버변수를 사용하던 코드 수정
    손형식 참고

--- app/src/main/java/com/paeparo/paeparo_mobile/adapter/MyHomeLikeAdapter.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/adapter/MyHomeLikeAdapter.kt
@@ -5,14 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.paeparo.paeparo_mobile.databinding.ItemMyhomeLikeBinding
 import com.paeparo.paeparo_mobile.model.Post
-import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.util.Date
 
-class MyHomeLikeAdapter(private var likedPostList:MutableList<Post>):RecyclerView.Adapter<MyHomeLikeAdapter.MyHomeLikeViewHoloder>() {
+class MyHomeLikeAdapter(private var likedPostList: MutableList<Post>) :
+    RecyclerView.Adapter<MyHomeLikeAdapter.MyHomeLikeViewHoloder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHomeLikeViewHoloder {
-        val binding = ItemMyhomeLikeBinding.inflate(LayoutInflater.from(parent.context),parent,false,)
+        val binding =
+            ItemMyhomeLikeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyHomeLikeViewHoloder(binding)
     }
 
@@ -22,14 +21,14 @@ class MyHomeLikeAdapter(private var likedPostList:MutableList<Post>):RecyclerVie
         holder.bind(likedPostList[position])
     }
 
-    class MyHomeLikeViewHoloder(private  val binding: ItemMyhomeLikeBinding):RecyclerView.ViewHolder(binding.root) {
+    class MyHomeLikeViewHoloder(private val binding: ItemMyhomeLikeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(post: Post){
-            with(binding){
+        fun bind(post: Post) {
+            with(binding) {
                 title.text = post.title // 제목
                 date.text = post.createdAt.toDate().toString()
                 likeNum.text = post.likes.toString() // 좋아요 수
-                commentNum.text = post.comments.toString() //코멘트 수
             }
         }
     }
