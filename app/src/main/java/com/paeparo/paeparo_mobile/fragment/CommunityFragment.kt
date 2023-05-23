5ae44a8caf7f77f204ef9552c16f6ad3050a7788
Sohn Young Jin <sonkim1001@naver.com>
2023-05-24 02:21:22 +0900

    feat: CommunityFragment 구현 및 게시물 목록 불러오기 기능 구현

---
+++ app/src/main/java/com/paeparo/paeparo_mobile/fragment/CommunityFragment.kt
@@ -0,0 +1,106 @@
+package com.paeparo.paeparo_mobile.fragment
+
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.paeparo.paeparo_mobile.adapter.PostAdapter
+import com.paeparo.paeparo_mobile.databinding.FragmentCommunityBinding
+import com.paeparo.paeparo_mobile.model.PostViewModel
+
+class CommunityFragment : Fragment() {
+    private var _binding: FragmentCommunityBinding? = null
+    private val binding get() = _binding!!
+
+    /**
+     * PostViewModel
+     */
+    private lateinit var postViewModel: PostViewModel
+
+    /**
+     * PostAdapter
+     */
+    private lateinit var postAdapter: PostAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupPostViewModel()
+        setupPostAdapter()
+
+        postViewModel.loadPosts()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /**
+     * PostViewModel 초기화 함수
+     */
+    private fun setupPostViewModel() {
+        postViewModel = ViewModelProvider(this)[PostViewModel::class.java]
+        postViewModel.newPostList.observe(viewLifecycleOwner) { postList ->
+            postAdapter.addPostList(postList)
+        }
+    }
+
+    /**
+     * PostAdapter 초기화 함수
+     */
+    private fun setupPostAdapter() {
+        postAdapter = PostAdapter()
+        binding.rvCommunityPostList.addItemDecoration(CustomItemDecoration(16))
+        binding.rvCommunityPostList.layoutManager =
+            StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.rvCommunityPostList.adapter = postAdapter
+
+        binding.rvCommunityPostList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val layoutManager = recyclerView.layoutManager as StaggeredGridLayoutManager
+                val lastVisibleItem =
+                    layoutManager.findLastVisibleItemPositions(null).maxOrNull() ?: 0
+
+                if (!postViewModel.loading.value!! && layoutManager.itemCount <= (lastVisibleItem + 4)) {
+                    // 현재 새로운 Post 목록을 로딩 중이 아니고 스크롤이 끝에 도달했을 경우, 새로운 Post 목록 로딩
+                    postViewModel.loadPosts()
+                }
+            }
+        })
+    }
+
+    class CustomItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            outRect.left = space
+            outRect.right = space
+            outRect.bottom = space
+
+            // 첫 번째 아이템이라면 위쪽 간격도 추가한다.
+            if (parent.getChildAdapterPosition(view) > 1) {
+                outRect.top = space
+            }
+        }
+    }
+}
