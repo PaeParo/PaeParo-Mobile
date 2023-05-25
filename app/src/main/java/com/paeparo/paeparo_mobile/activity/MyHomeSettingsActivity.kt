ba1528a42f85ed8a4c8f32b6a62ef15e26d5fbd0
Blen <blenbre777@naver.com>
2023-05-25 17:43:12 +0900

    fix:setting 뒤로가기 수정 및 style 변경

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MyHomeSettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.paeparo.paeparo_mobile.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.paeparo.paeparo_mobile.constant.SharedPreferencesKey
 import com.paeparo.paeparo_mobile.databinding.ActivityMyHomeSettingsBinding
@@ -19,6 +21,8 @@ class MyHomeSettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMyHomeSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val builder = AlertDialog.Builder(this) //다이어로그 창
         loadData()
 
         //초기 세팅 추후에 코드 바꿀 예정
@@ -38,16 +42,32 @@ class MyHomeSettingsActivity : AppCompatActivity() {
                 SharedPreferencesManager.set(SharedPreferencesKey.KEY_SETTINGS_ALARM,false)
             }
         }
-        binding.user.setOnClickListener {
-            startActivity(Intent(this, MyHomeSettingsUserActivity::class.java))
+
+        binding.userDetail.setText("회원탈퇴를 합니다.")
+
+        binding.userLayout.setOnClickListener {
+            //startActivity(Intent(this, MyHomeSettingsUserActivity::class.java))
+            builder.setTitle("회원탈퇴를 하시겠습니까?")
+                .setMessage("일정 및 댓글 모든 정보가 삭제될것입니다.")
+                .setPositiveButton("확인",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        //DB 삭제
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        ActivityCompat.finishAffinity(this)
+                    })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        //빈칸
+                    })
+            builder.show()
         }
 
-        binding.logoutButton.setOnClickListener(){
+        binding.logout.setOnClickListener(){
             FirebaseManager.logoutWithGoogle(this)
             startActivity(Intent(this, LoginActivity::class.java))
             ActivityCompat.finishAffinity(this)
         }
-        binding.settings.setOnClickListener {
+        binding.back.setOnClickListener {
             finish()
         }
         binding.settings.setText("설정")
