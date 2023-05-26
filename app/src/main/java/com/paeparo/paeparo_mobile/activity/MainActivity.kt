56017643bdc89ae99f58976d8efb2d7f2e3cc011
Sohn Young Jin <sonkim1001@naver.com>
2023-05-26 14:02:08 +0900

    feat: Fragment가 불필요하게 재생성되던 로직 수정 및 FadeIn/Out 애니메이션 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/MainActivity.kt
@@ -13,27 +13,33 @@ import com.paeparo.paeparo_mobile.fragment.TripFragment
 class MainActivity : AppCompatActivity(), OnPostFragmentInteractionListener {
     private lateinit var binding: ActivityMainBinding
     private var currentTabId: Int = -1
+    private val communityFragment = CommunityFragment()
+    private val tripFragment = TripFragment()
+    private val myHomeFragment = MyHomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Navigation 선택 이벤트
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fl_main_view, tripFragment)
+            .commit()
+
         binding.bvMainBottomNavigation.setOnItemSelectedListener { item ->
             if (item.itemId != currentTabId) {
                 val ft = supportFragmentManager.beginTransaction()
                 when (item.itemId) {
                     R.id.community_fragment -> {
-                        ft.replace(R.id.fl_main_view, CommunityFragment()).commit()
+                        ft.replace(R.id.fl_main_view, communityFragment).commit()
                     }
 
                     R.id.trip_fragment -> {
-                        ft.replace(R.id.fl_main_view, TripFragment()).commit()
+                        ft.replace(R.id.fl_main_view, tripFragment).commit()
                     }
 
                     R.id.mypage_fragment -> {
-                        ft.replace(R.id.fl_main_view, MyHomeFragment()).commit()
+                        ft.replace(R.id.fl_main_view, myHomeFragment).commit()
                     }
                 }
                 currentTabId = item.itemId
@@ -42,9 +48,7 @@ class MainActivity : AppCompatActivity(), OnPostFragmentInteractionListener {
                 false
             }
         }
-
         binding.bvMainBottomNavigation.selectedItemId = R.id.trip_fragment
-        //    testRetrofit()
     }
 
     override fun onPostFragmentDisplayed() {
