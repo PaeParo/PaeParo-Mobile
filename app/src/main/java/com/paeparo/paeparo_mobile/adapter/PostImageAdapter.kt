bdd614a6079a778da7efe7cd5d87453b11720b78
Sohn Young Jin <sonkim1001@naver.com>
2023-05-26 13:14:50 +0900

    docs: 주석 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/adapter/PostImageAdapter.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/adapter/PostImageAdapter.kt
@@ -6,16 +6,15 @@ import com.paeparo.paeparo_mobile.databinding.ItemPostImageBinding
 import com.paeparo.paeparo_mobile.util.ImageUtil
 import com.smarteist.autoimageslider.SliderViewAdapter
 
-
+/**
+ * PostImageAdapter class
+ *
+ * 게시물 세부정보 화면에서 게시물 이미지 목록을 관리하는 Adapter 클래스
+ *
+ * @property imageUrlList 표시할 이미지 URL 목록
+ */
 class PostImageAdapter(private val imageUrlList: MutableList<String>) :
     SliderViewAdapter<PostImageAdapter.PostImageViewHolder>() {
-
-    inner class PostImageViewHolder(private val binding: ItemPostImageBinding) :
-        ViewHolder(binding.root) {
-        fun bind(imageUrl: String) {
-            ImageUtil.displayImageFromUrl(binding.ivItemPostImage, imageUrl)
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup): PostImageViewHolder {
         return PostImageViewHolder(
@@ -29,5 +28,17 @@ class PostImageAdapter(private val imageUrlList: MutableList<String>) :
 
     override fun getCount(): Int {
         return imageUrlList.size
+    }
+    
+    /**
+     * 게시물 이미지를 담고 있는 ViewHolder
+     *
+     * @property binding ItemPostImageBinding
+     */
+    inner class PostImageViewHolder(private val binding: ItemPostImageBinding) :
+        ViewHolder(binding.root) {
+        fun bind(imageUrl: String) {
+            ImageUtil.displayImageFromUrl(binding.ivItemPostImage, imageUrl)
+        }
     }
 }
