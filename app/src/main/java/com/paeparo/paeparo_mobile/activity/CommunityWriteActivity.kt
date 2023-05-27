774925e375773ae2c850faf3b7d4a3083bf2b54c
Blen <blenbre777@naver.com>
2023-05-27 15:57:29 +0900

    feat:communityWriteActivity 글쓰기 기능 추가

--- app/src/main/java/com/paeparo/paeparo_mobile/activity/CommunityWriteActivity.kt
+++ app/src/main/java/com/paeparo/paeparo_mobile/activity/CommunityWriteActivity.kt
@@ -1,21 +1,22 @@
 package com.paeparo.paeparo_mobile.activity
 
 import android.app.Activity
-import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
-import android.widget.HorizontalScrollView
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
 import com.paeparo.paeparo_mobile.adapter.CommunityImagesAdapter
 import com.paeparo.paeparo_mobile.adapter.CommunityTagAdapter
 import com.paeparo.paeparo_mobile.databinding.ActivityCommunityWriteBinding
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import com.paeparo.paeparo_mobile.manager.FirebaseManager
+import com.paeparo.paeparo_mobile.model.Post
+import kotlinx.coroutines.launch
 
 
 class CommunityWriteActivity : AppCompatActivity() {
@@ -37,14 +38,31 @@ class CommunityWriteActivity : AppCompatActivity() {
         binding.communityTagRecyclerView.adapter = tagAdapter
 
         //이미지 뷰 adapter
-        binding.communityPictureRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        binding.communityPictureRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.communityPictureRecyclerView.adapter = pictureAdapter
 
 
-            //글쓰기 눌렀을 경우 모든 정보가 넘어가야 함
+        //상단 제어
         binding.write.setOnClickListener {
-
+            lifecycleScope.launch {
+                val post = Post(
+                    title = binding.title.toString(),
+                    description = binding.content.toString(),
+                    createdAt = Timestamp.now(),
+                    tags = tagAdapter.getTagList() as MutableList<String>,
+                )
+                val result = FirebaseManager.createPost(
+                    post,pictureAdapter.getImageList()
+                )
             }
+        }
+        binding.back.setOnClickListener {
+            finish()
+        }
+        binding.community.text = "글쓰기"
+
+        //태그 추가
         binding.cvCommunityWriteAddTag.setOnClickListener {
             val inputEditText = EditText(this@CommunityWriteActivity)
             builder.setTitle("태그 추가")
@@ -57,15 +75,13 @@ class CommunityWriteActivity : AppCompatActivity() {
                 .setNegativeButton("취소", null)
             builder.show()
         }
+
+        //이미지 추가
         binding.pictureTake.setOnClickListener {
             val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
             startActivityForResult(intent, OPEN_GALLERY)
         }
-        binding.back.setOnClickListener {
-            finish()
-        }
-        binding.community.text = "글쓰기"
     }
 
     /**
